b75a275b02cfbaa600bdf2b9dc57009d559e6b77
iamhvsharma <itshvsharma@gmail.com>
2025-11-03 23:05:11 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on :", PORT);
+})
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        msg: "Home page"
+    })
+})
