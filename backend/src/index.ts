a9cc37fe5d5979261d84887dc5de2fc46a7f69a4
iamhvsharma <itshvsharma@gmail.com>
2025-11-09 22:31:03 +0530

    mongo setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db";
 import { app } from "./app";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 dotenv.config();
 
 connectDB()
   .then(() => {
-    app.on('error', (error) => {
-      console.log("ERR: ", error);
-      throw error;
-    });
+    app.use(
+      (
+        err: ErrorRequestHandler,
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log("ERR: ", err);
+        res.status(500).send("Something broke!");
+      }
+    );
 
     app.listen(process.env.PORT || 8080, () => {
       console.log(`Server is running at PORT: ${process.env.PORT}`);
